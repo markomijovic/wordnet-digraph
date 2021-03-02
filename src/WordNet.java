6fa3d0ae29a655bc18f8df567bd7da1d937fca8a
Marko Mijovic <mijovicmarko11@gmail.com>
2021-03-01 22:16:59 -0700

    digraph implementation

--- src/WordNet.java
+++ src/WordNet.java
@@ -1,24 +1,29 @@
-import edu.princeton.cs.algs4.In;
-
+import edu.princeton.cs.algs4.Digraph;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
 public class WordNet
 {
-    private List<List<String>> synsets = new ArrayList<List<String>>();
-    private List<List<Integer>> hypernyms = new ArrayList<List<Integer>>();
+    private HashMap<Integer, String> synsets = new HashMap<Integer, String>();
     private String fileName_syn;
     private String fileName_hyp;
+    private int size;
+    private Digraph graph;
+
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms)
     {
         // check that inputs are not null
         if (isNull(synsets) || isNull(hypernyms)) throw new IllegalArgumentException();
-        this.fileName_syn = synsets;
-        this.fileName_hyp = hypernyms;
+        fileName_syn = synsets;
+        fileName_hyp = hypernyms;
         fillSynsets();
-        fillHypernyms();
+        size = this.synsets.size();
+        graph = new Digraph(size);
+        makeGraph();
     }
     /*
     // returns all WordNet nouns
@@ -49,11 +54,7 @@ public class WordNet
             {
                 List<String> words = new ArrayList<String>();
                 String[] tokens = line.split(",");
-                for (String word : tokens[1].split(" "))
-                {
-                    words.add(word);
-                }
-                this.synsets.add(words);
+                this.synsets.put(Integer.parseInt(tokens[0]), tokens[1]);
             }
         } catch (Exception e)
         {
@@ -63,6 +64,7 @@ public class WordNet
 
     /* Fill this.hypernyms such that hypernyms[i] = [int1, int2,...]
     where i is the synset ID and int1, int2 are the corresponding hypernyms' synset IDs */
+    /* NOT NEEDED ATM
     private void fillHypernyms()
     {
         try(BufferedReader br = new BufferedReader(new FileReader(this.fileName_hyp)))
@@ -81,6 +83,28 @@ public class WordNet
         {
             e.printStackTrace();
         }
+    } */
+    /*
+    Read the hypernyms file and create the Digraph
+     */
+    private void makeGraph()
+    {
+        try(BufferedReader br = new BufferedReader(new FileReader(this.fileName_hyp)))
+        {
+            for(String line; (line = br.readLine()) != null; )
+            {
+                String[] ids = line.split(",");
+                int v = Integer.parseInt(ids[0]);
+                for (int k = 1; k < ids.length; k++)
+                {
+                    int w = Integer.parseInt(ids[k]);
+                    graph.addEdge(v, w);
+                }
+            }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     // check if object is null
@@ -92,6 +116,5 @@ public class WordNet
     {
         WordNet myWN = new WordNet("C:\\Users\\Marko\\Desktop\\School\\CS - Personal\\Coursera - Princeton - Algorithms2\\Week1\\WordNet\\data\\synsets.txt",
                 "C:\\Users\\Marko\\Desktop\\School\\CS - Personal\\Coursera - Princeton - Algorithms2\\Week1\\WordNet\\data\\hypernyms.txt");
-        System.out.println(myWN.hypernyms);
     }
 }
