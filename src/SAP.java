1fbd3e141f1632fe6c3358852a83a10f554ec14a
Marko Mijovic <mijovicmarko11@gmail.com>
2021-03-06 01:33:47 -0700

    Ancestor search implemented

--- src/SAP.java
+++ src/SAP.java
@@ -1,7 +1,10 @@
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
 
 public class SAP
 {
+    private final Digraph graph;
+    private BreadthFirstDirectedPaths pV, pW;
 
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G)
@@ -10,18 +13,47 @@ public class SAP
         {
             throw new IllegalArgumentException();
         }
+        this.graph = G;
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w)
     {
-
+        if (v > graph.V() || w > graph.V()) throw new IllegalArgumentException();
+        pV = new BreadthFirstDirectedPaths(graph, v);
+        pW = new BreadthFirstDirectedPaths(graph, w);
+        int min_dist = Integer.MAX_VALUE;
+        for (int i = 0; i < graph.V(); i++)
+        {
+            if (pV.hasPathTo(i) && pW.hasPathTo(i))
+            {
+                min_dist = Math.min(min_dist, pV.distTo(i)+pW.distTo(i));
+            }
+        }
+        if (min_dist == Integer.MAX_VALUE) return -1;
+        return min_dist;
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w)
     {
-
+        if (v > graph.V() || w > graph.V()) throw new IllegalArgumentException();
+        pV = new BreadthFirstDirectedPaths(graph, v);
+        pW = new BreadthFirstDirectedPaths(graph, w);
+        int dist = Integer.MAX_VALUE;
+        int ancestor = -1;
+        for (int i = 0; i < graph.V(); i++)
+        {
+            if (pV.hasPathTo(i) && pW.hasPathTo(i))
+            {
+                if (pV.distTo(i) + pW.distTo(i) < dist)
+                {
+                    dist = pV.distTo(i) + pW.distTo(i);
+                    ancestor = i;
+                }
+            }
+        }
+        return ancestor;
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
